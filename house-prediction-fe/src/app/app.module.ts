f0406fd0a69973dc6137b43606f304b6f7919f8e
OlenaPeretiatko <olenka.peretyatko@gmail.com>
2024-03-14 17:57:55 -0700

    Added ng2-charts

--- house-prediction-fe/src/app/app.module.ts
+++ house-prediction-fe/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,12 +16,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NavigationComponent,
     RouterModule.forRoot([
       {
         path: '',
-        loadChildren: () =>
-          import('./price-prediction-form/price-prediction-form.module').then(
-            (m) => m.PricePredictionFormModule
+        loadComponent: () =>
+          import('./components/home/home.component').then(
+            (c) => c.HomeComponent
           ),
       },
     ]),
